a0546620bb92f5f6c576686c999f163401c84ebe
leticiacabrera <54179710+leticiacabrera@users.noreply.github.com>
2019-09-11 14:40:04 -0300

    Prueba login google, funcionaráPrueba login google, funcionará??

--- js/Log in.js
+++ js/Log in.js
@@ -4,3 +4,36 @@ function set(){
     /*Guardando los datos en el LocalStorage*/
     localStorage.setItem("Username", email);
 };
+
+/*google login*/
+
+//var profile = googleUser . getBasicProfile ();
+//var id_token = googleUser . getAuthResponse (). id_token ;
+
+function onSignIn ( googleUser ) { 
+    var profile = googleUser . getBasicProfile ();
+    document.getElementById('profileinfo').innerHTML = profile.getName() + "<br>"
+        + "<a href = 'mailto: '" + profile.getEmail() + "'>" + profile.getEmail() + "</a>" + "</br>"
+        + "<img scr='" + profile.getImageUrl() + "'/>";
+}
+
+function singOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.singOut().then(function () {
+        document.getElementById('profileinfo').innerHTML = "";
+    });
+}
+// Datos útiles para sus scripts del lado del cliente: var profile = googleUser . getBasicProfile ();         
+//consola . log ( "ID:" + perfil . getId ()); // ¡No envíes esto directamente a tu servidor!         
+//consola . log ( 'Nombre completo:' + perfil . getName ());         
+//consola . log ( 'Nombre de pila:'+ perfil . getGivenName ()); 
+
+  // consola . log ( 'Nombre de familia:' + perfil . getFamilyName ());         
+  // consola . log ( "URL de imagen:" + perfil . getImageUrl ());         
+  // consola . log ( "Correo electrónico:" + perfil . getEmail ()); 
+   
+   // El token de ID que debes pasar a tu backend: var id_token = googleUser . getAuthResponse (). id_token ;
+
+   
+  // consola . log ( "ID Token:" + id_token ); } 
+
