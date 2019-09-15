a0a431d2c6ec3e5f1348acfac4e5a812acecc6e3
leticiacabrera <54179710+leticiacabrera@users.noreply.github.com>
2019-09-15 00:49:34 -0300

    Se agregó menú desplegable a dos archivos

--- js/Log in.js
+++ js/Log in.js
@@ -7,7 +7,23 @@ function set(){
 
 /*google login*/
 
-//var profile = googleUser . getBasicProfile ();
+
+function onSignIn(googleUser) {
+    // Useful data for your client-side scripts:
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
+
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+}
+
+/*var profile = googleUser . getBasicProfile ();
 var id_token = googleUser . getAuthResponse (). id_token ;
 
 function onSignIn ( googleUser ) { 
@@ -24,5 +40,5 @@ function onSignIn ( googleUser ) {
    // El token de ID que debes pasar a tu backend: var id_token = googleUser . getAuthResponse (). id_token ;
 
    
-   consola . log ( "ID Token:" + id_token ); } 
+   consola . log ( "ID Token:" + id_token ); }  */
 
