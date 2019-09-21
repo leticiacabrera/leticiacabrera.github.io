aa6c8ad6de8e406a0dd8feeb39841963aeed8d62
leticiacabrera <55068359+leticiacabrera@users.noreply.github.com>
2019-09-20 22:37:09 -0300

    solo log in google

--- js/Log in.js
+++ js/Log in.js
@@ -1,9 +1,3 @@
-/*function set(){
-    /*Captura de datos escrito en el input*/        
-   /* var email = document.getElementById("inputUsername").value;
-    /*Guardando los datos en el LocalStorage*/
-     /*localStorage.setItem("Username", email);
-};
 
 /*google login*/
 
