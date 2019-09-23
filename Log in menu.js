78cb7e83bbb42b7ee397a00dafefc3f4fe1b0fe6
leticiacabrera <55068359+leticiacabrera@users.noreply.github.com>
2019-09-23 18:31:15 -0300

    me equivoque de carpeta

--- Log in menu.js
+++
@@ -1,5 +0,0 @@
-function get(){
-    var name = localStorage.getItem("g-signin2"); //Obtener datos almacenados
-    document.getElementById("dropdownMenuButton").innerHTML = name; //Mostrar datos almacenados
-};
-get();
